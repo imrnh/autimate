30624f016c47b18e70d27306d05db0775600c15d
Muhtasir Imran <mu.imran2002@gmail.com>
2024-09-22 15:20:06 +0600

    Implemented AI

---
+++ ai/text/main.py
@@ -0,0 +1,23 @@
+import uuid
+import onnxruntime as ort
+import numpy as np
+
+
+def autism_prediction(model_path, data_array):
+    # Data preperation
+    data_array = np.array(data_array).astype(np.float32)
+    data_array = {'input': data_array}
+
+    # Model building.
+    ort_session = ort.InferenceSession(model_path)
+    output = ort_session.run(None, data_array)
+    
+    return output
+
+
+
+def main(data_array):
+    return autism_prediction("q10_autism_detection_model.onnx", data_array)
+
+
+# main([ 1., 0., 1., 1., 1., 0., 1., 1., 1., 1., 0., 8., 26., 0.])
---
+++ ai/text/readme.md
@@ -0,0 +1,6 @@
+array holds this values:
+
+a1 - a10, autism_in_family, score, age, gender    
+
+[1= male, 0 = female]
+score = sum of all the scores combined. [in 1-6 range for all q]
