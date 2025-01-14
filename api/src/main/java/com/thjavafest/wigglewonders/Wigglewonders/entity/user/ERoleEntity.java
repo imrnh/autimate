5bc9be1fd1b44914a44edd9c55f62da7948cc578
Muhtasir Imran <mu.imran2002@gmail.com>
2025-01-14 15:42:52 +0600

    Reorganie: removed drafted `api` folder

--- api/src/main/java/com/thjavafest/wigglewonders/Wigglewonders/entity/user/ERoleEntity.java
+++
@@ -1,8 +0,0 @@
-package com.thjavafest.wigglewonders.Wigglewonders.entity.user;
-
-
-public enum ERoleEntity {
-  ROLE_USER,
-  ROLE_MODERATOR,
-  ROLE_ADMIN
-}
--- api/src/main/java/com/thjavafest/wigglewonders/Wigglewonders/repo/QuestionExamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thjavafest.wigglewonders.Wigglewonders.repo;
-
-import com.thjavafest.wigglewonders.Wigglewonders.entity.QuestionExamEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface QuestionExamRepository extends JpaRepository<QuestionExamEntity, Long> {
-}
-
